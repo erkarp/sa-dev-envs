1d51b9cc44f9822ec6a4db5c0ccaaaae9f00a936
ekarp <emilyrkarp@gmail.com>
2018-09-15 14:14:04 -0400

    options page sets default staging environment

--- options.js
+++ options.js
@@ -1,18 +1,29 @@
-// Entire file from tutorial https://developer.chrome.com/extensions/getstarted
+const optionsFormElem = document.getElementById('options');
 
-let page = document.getElementById('buttonDiv');
-const kButtonColors = ['#3aa757', '#e8453c', '#f9bb2d', '#4688f1'];
+chrome.storage.sync.get(['origins', 'shortcutMap'], function(data) {
+  data.origins.forEach(function(origin) {
 
-function constructOptions(kButtonColors) {
-  for (let item of kButtonColors) {
-    let button = document.createElement('button');
-    button.style.backgroundColor = item;
-    button.addEventListener('click', function() {
-      chrome.storage.sync.set({color: item}, function() {
-        console.log('color is ' + item);
-      })
-    });
-    page.appendChild(button);
-  }
-}
-constructOptions(kButtonColors);
+    if (origin.name !== 'local' && origin.name !== 'sa.com') {
+      let inputElem = document.createElement('input');
+
+      inputElem.setAttribute('type', 'radio');
+      inputElem.setAttribute('name', 'defaultStagingEnvironment');
+
+      if (origin.name === data.shortcutMap.view_staging) {
+        inputElem.setAttribute('checked', true);
+      }
+
+      inputElem.addEventListener('click', function() {
+        data.shortcutMap.view_staging = origin.name;
+        chrome.storage.sync.set({shortcutMap: data.shortcutMap});
+      });
+
+      // Create label for radio input
+      let labelElem = document.createElement('label');
+      labelElem.append(origin.name);
+
+      labelElem.appendChild(inputElem);
+      optionsFormElem.appendChild(labelElem);
+    }
+  });
+})
